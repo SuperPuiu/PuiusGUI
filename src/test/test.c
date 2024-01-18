9c0d37e4136a6e6e484dd31a5db13bc43b9754df
SuperPuiu <97047469+SuperPuiu@users.noreply.github.com>
2024-01-18 17:20:21 +0200

    Update test.c
    This update includes more usage of properties that PuiusGUI provide.

--- src/test/test.c
+++ src/test/test.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../include/PuiusGUI.h"
+#include "include/PuiusGUI.h"
 
 struct inputStruct input;
 int game_running = 1;
+
+void buttonCallback(int guiIndex) {
+    guiArray[guiIndex].Visible = 0;
+}
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -22,16 +26,26 @@ int main() {
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     initLayer(renderer, window);
 
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_MUL);
+
     int index1 = ConstructGUI(TEXTBOX);
     int index2 = ConstructGUI(TEXTLABEL);
-    ConstructGUI(TEXTBUTTON);
+    int index3 = ConstructGUI(TEXTBUTTON);
 
     guiArray[index2].PositionX = 200;
     guiArray[index1].PositionY = 100;
     guiArray[index1].PositionX = 100;
     guiArray[index2].PositionY = 200;
 
-    guiArray[index2].TextColor = initColor3(0, 100, 255);
+    guiArray[index2].TextColor = initColor3(0, 100, 255, 255);
+    guiArray[index2].BackgroundColor = initColor3(0, 0, 0, 255);
+
+    guiArray[index2].Text = "Label";
+    guiArray[index3].Text = "Button";
+
+    guiArray[index3].MouseDown = buttonCallback;
+    guiArray[index1].TextXAlignment = X_CENTER;
+    guiArray[index1].TextYAlignment = Y_CENTER;
 
     guiArray[index1].SizeY = 300;
     updateAllGUI();
