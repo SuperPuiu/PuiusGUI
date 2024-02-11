52e5df8105ec7a75b356b322ec4aa78e0e5d8e45
SuperPuiu <97047469+SuperPuiu@users.noreply.github.com>
2024-02-11 21:02:19 +0200

    Update test.c
    Updated so it tests more properties.

--- src/test/test.c
+++ src/test/test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include "include/PuiusGUI.h"
+#include "../include/PuiusGUI.h"
 
 void buttonCallback(int guiIndex) {
     GuiArray[guiIndex].Visible = 0;
@@ -26,6 +26,7 @@ int main() {
 
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_MUL);
 
+    int index4 = ConstructGUI(TEXTLABEL, 10, 10);
     int index1 = ConstructGUI(TEXTBOX, 100, 100);
     int index2 = ConstructGUI(TEXTLABEL, 200, 200);
     int index3 = ConstructGUI(TEXTBUTTON, 0, 0);
@@ -46,6 +47,13 @@ int main() {
     GuiArray[index3].OutlineSize = 1;
 
     GuiArray[index1].SizeY = 300;
+
+    GuiArray[index2].Parent = index4;
+    GuiArray[index2].Zindex = 3;
+    GuiArray[index4].Zindex = 1;
+    GuiArray[index4].BackgroundColor = InitColor3(0, 0, 0, 255);
+    GuiArray[index4].TextColor = InitColor3(255, 255, 255, 255);
+
     UpdateAllGUI();
 
     while(Running == 1) {
